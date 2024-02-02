2fbb2046b62ce2b1300ddea7701eb10334b7d10a
Akshay <saxena.akshay571996@gmail.com>
2024-02-02 18:59:15 +0530

    updated

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const { Sequelize,Model,DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize('Employee', 'postgres', 'Esoft123', {
   logging:false,
-    host: 'http://44.221.1.243/',
+    host: '44.221.1.243',
     dialect:"postgres"/* one of 'mysql' |  | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
   });
 
